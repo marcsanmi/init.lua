f2315fb02f063ab1abaf29f84eff6ffd1fcfedd4
Marc Sanmiquel <marcsanmiquel@gmail.com>
2024-05-30 16:41:19 +0200

    Add gitsings missing hunk basics

--- lua/akerman/lazy/gitsigns.lua
+++ lua/akerman/lazy/gitsigns.lua
@@ -6,10 +6,16 @@ return {
             on_attach = function(bufnr)
                 local gs = package.loaded.gitsigns
 
-                vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame, {})
-                vim.keymap.set('n', '<leader>hd', gs.diffthis)
-
+                vim.keymap.set({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = 'Reset Hunk' })
+                vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo Stage Hunk' })
+                vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview Hunk' })
+                vim.keymap.set('n', '<leader>hb', function() gs.blame_line { full = true } end,
+                    { desc = 'Blame Line Full' })
+                vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'Toggle Current Line Blame' })
+                vim.keymap.set('n', '<leader>hd', gs.diffthis, { desc = 'Diff This' })
+                vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'Diff This ~' })
             end
         });
     end
 }
+
